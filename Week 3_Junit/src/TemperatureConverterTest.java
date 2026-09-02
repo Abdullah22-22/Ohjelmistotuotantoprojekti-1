7db6ea67918c1a10959ae59eaaf649ab81bdf9b3
Abdullah22-22 <abdullahaltameemi28@gmail.com>
2026-09-02 18:00:29 +0300

    feat: add Week 3_Junit

---
+++ Week 3_Junit/src/TemperatureConverterTest.java
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class TemperatureConverterTest {
+
+    TemperatureConverter converter = new TemperatureConverter();
+
+    @Test
+    void fahrenheitToCelsius() {
+        assertEquals(0, converter.fahrenheitToCelsius(32), 0.01);
+        assertEquals(100, converter.fahrenheitToCelsius(212), 0.01);
+        assertEquals(-40, converter.fahrenheitToCelsius(-40), 0.01);
+    }
+
+    @Test
+    void celsiusToFahrenheit() {
+        assertEquals(32, converter.celsiusToFahrenheit(0), 0.01);
+        assertEquals(212, converter.celsiusToFahrenheit(100), 0.01);
+        assertEquals(-40, converter.celsiusToFahrenheit(-40), 0.01);
+    }
+
+    @Test
+    void isExtremeTemperature() {
+        assertTrue(converter.isExtremeTemperature(-41));
+        assertTrue(converter.isExtremeTemperature(51));
+        assertFalse(converter.isExtremeTemperature(-40));
+        assertFalse(converter.isExtremeTemperature(50));
+        assertFalse(converter.isExtremeTemperature(25));
+    }
+}
