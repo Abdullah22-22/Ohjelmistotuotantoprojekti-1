5a660c9f21a620388a6d95326f0081e0d9a974f0
Abdullah22-22 <abdullahaltameemi28@gmail.com>
2026-09-23 10:26:07 +0300

    feat: add Docker build and push stages

--- Week 3_Junit/src/main/java/TemperatureConverter.java
+++ Week 3_Junit/src/main/java/TemperatureConverter.java
@@ -15,4 +15,15 @@ public class TemperatureConverter {
     public double kelvinToCelsius(double kelvin) {
         return kelvin - 273.15;
     }
+
+    public static void main(String[] args) {
+        TemperatureConverter converter = new TemperatureConverter();
+
+        System.out.println("=== Temperature Converter ===");
+        System.out.println("32 F  = " + converter.fahrenheitToCelsius(32) + " C");
+        System.out.println("100 C = " + converter.celsiusToFahrenheit(100) + " F");
+        System.out.println("300 K = " + converter.kelvinToCelsius(300) + " C");
+        System.out.println("Is 60 C extreme? " + converter.isExtremeTemperature(60));
+        System.out.println("=============================");
+    }
 }
